c153f2dc28966fca29ec539ff605429e9349455d
anupam1608 <anupam.misra@students.iiit.ac.in>
2019-11-10 17:27:55 +0530

    Structures

--- client.cpp
+++ client.cpp
@@ -17,10 +17,47 @@ struct node_data
     string ip;
     string port;
 };
+struct node_data getdefaul_node()
+{
+    struct node_data defaul;
+    defaul.nodeid="...";
+    defaul.ip=".";
+    defaul.port="..";
+    return defaul;
+
+}
+
+ vector<vector<struct node_data>> get_table(){
+    vector<vector<struct node_data>>v(8,vector<struct node_data>(16,getdefaul_node()));
+    return  v;
+ }
+
+std::vector<struct node_data> getleaf()
+{
+    std::vector<struct node_data> leafset(4,getdefaul_node());
+    return leafset;
+}
+
+std::vector<struct node_data> getneighbour()
+{
+    std::vector<struct node_data> neighbourhoodset(4,getdefaul_node());
+    return neighbourhoodset;
+}
+
+struct node_structur{
+    string ip;
+    string port;
+    string nodeid;
+    struct node_data defaul = getdefaul_node();
+    int n=4;
+    vector<vector<struct node_data>> routing_table=get_table();
+    std::vector<struct node_data> leafset=getleaf();
+    std::vector<struct node_data> neighbourhoodset=getneighbour();
+    
 
 
-vector<vector<node_data>> routing_table;
-vector<node_data> leaf_set;
+
+};
 int b;
 
 void *server(void * arg)
@@ -33,7 +70,7 @@ int main(int argc,char **argv)
     bool bootstrapnode=false;
     string new_ip;
     string new_port;
-
+    
     string node_ip;
     string node_port;
 
